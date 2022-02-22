4a60b12eb9cb35408d59025de94936e2456ba906
MIkaela Braga <bragamikaela@gmail.com>
2022-02-22 14:29:55 -0300

    Iniciando o projeto

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import app from './app';
 
 const PORT = 3000;
 
+// iniciando o projeto
+app.get('/', (_req, _res) => {});
+
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
