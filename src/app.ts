9ed55bde70694919582368e21e51ed13d666b160
MIkaela Braga <bragamikaela@gmail.com>
2022-02-22 18:34:11 -0300

    Configura variaveis de ambiente e inicia conexao com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,6 @@ const app = express();
 
 app.use(express.json());
 
+// rotas devem estar aqui
+
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import app from './app';
 
 const PORT = 3000;
 
-// iniciando o projeto
-app.get('/', (_req, _res) => {});
-
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
