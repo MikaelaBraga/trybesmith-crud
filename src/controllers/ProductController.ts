34c173eae9d326e151409dc27d104783fdc38215
MIkaela Braga <bragamikaela@gmail.com>
2022-03-06 22:38:11 -0300

    Validaçoes dos inputs de products finalizados

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import rescue from 'express-rescue';
 import { createProduct } from '../services/ProductService';
+import authentication from '../middlewares/authentication';
+import validateJoiProduct from '../middlewares/validateJoiProduct';
 
 const routeProduct = Router();
 
 // verificar -> tipagem dos parametros req e res dando erro
-routeProduct.post('/', rescue(async (req, res) => {
+routeProduct.post('/', authentication, rescue(async (req, res) => {
+  validateJoiProduct(req.body);
   const product = await createProduct(req.body);
 
   return res.status(201).json({ item: product });
---
+++ src/middlewares/validateJoiProduct.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+import { Product } from '../interfaces/IProduct';
+
+const productSchema = Joi.object({
+  name: Joi.string().min(3).required().messages({
+    'any.required': 'Name is required',
+    'string.min': 'Name must be longer than 2 characters',
+    'string.base': 'Name must be a string',
+  }),
+  amount: Joi.string().min(8).required().messages({
+    'any.required': 'Amount is required',
+    'string.min': 'Amount must be longer than 2 characters',
+    'string.base': 'Amount must be a string',
+  }),
+});
+
+export default (object: Product) => {
+  const { error } = productSchema.validate(object);
+
+  if (error) throw error;
+};
